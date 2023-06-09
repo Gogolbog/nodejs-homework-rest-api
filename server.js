ddf2bcc1d04df399e39163b9dff1a7b74bb6b919
Gogolbog <111005996+Gogolbog@users.noreply.github.com>
2023-06-09 17:46:43 +0300

    Conected to Database

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Bogdan:L2sFPKxXsOK5DZuf@cluster0.1dfxsne.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3001);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
